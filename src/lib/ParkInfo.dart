d7f88065d36b8f2c0287002857eef66e0a5f323a
PedroCAB <up202006086@up.pt>
2022-05-18 21:16:47 +0100

    Added some code to ParkInfo FutureBuilder (Not Working)

--- src/lib/ParkInfo.dart
+++ src/lib/ParkInfo.dart
@@ -24,16 +24,37 @@ class ParkInfo extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Park Info'),
-      ),
-      body: new FutureBuilder(
+    return Container(
+      child: FutureBuilder(
         future: getParkData(),
-        builder: (BuildContext context, AsyncSnapshot<Map<String,String>> snapshot){
-          //How to get Map<String,String> from Future<Map<String,String>>
+        builder: (context, AsyncSnapshot<Map<String,String>> snapshot) {
+          switch( snapshot.connectionState){
+            case ConnectionState.none:
+              return Text("There is no connection");
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center( child: new CircularProgressIndicator());
+            case ConnectionState.done:
+              if (snapshot.data != null){
+                Map<String,String> myMap = snapshot.data as Map<String,String>; // transform your snapshot data in map
+                var keysList = myMap.keys.toList(); // getting all keys of your map into a list
+
+                return ListView.builder(
+                    itemExtent: 90,
+                    itemCount: myMap.length, // getting map length you can use keyList.length too
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          title: Text(myMap[index] as String),
+                      );
+                    }
+                );
+              }
+              // here your snapshot data is null so SharedPreferences has no data...
+              return Text("No data was loaded from SharedPreferences");
+          }//end switch
         },
       ),
     );
+
   }
 }
