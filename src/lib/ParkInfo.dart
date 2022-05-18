1f1f8978156687c55871db82697a1066b25c86f6
PedroCAB <up202006086@up.pt>
2022-05-18 18:22:03 +0100

    Some progress on parkAPI

--- src/lib/ParkInfo.dart
+++ src/lib/ParkInfo.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 
 const parkingUrl = 'https://sigarra.up.pt/feup/pt/instalacs_geral.ocupacao_parques';
 class ParkInfo extends StatelessWidget{
-  Future<Map> getParkData() async {
+  Future<Map<String,String>> getParkData() async {
     var response = await http.get(Uri.parse(parkingUrl));
     if (response.statusCode == 200) {
       var data = json.decode(response.body);
@@ -16,12 +16,24 @@ class ParkInfo extends StatelessWidget{
       throw Exception('Failed to read $parkingUrl');
     }
   }
-  
+  String mapToStr(Map<String,String> map){
+    String ans = "";
+    map.forEach((key, value) {ans += "$key : $value /n";});
+    return ans;
+  }
+
   @override
   Widget build(BuildContext context){
-    return Center(
-            child: Text("Test"
-            )
-        );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Park Info'),
+      ),
+      body: new FutureBuilder(
+        future: getParkData(),
+        builder: (BuildContext context, AsyncSnapshot<Map<String,String>> snapshot){
+          //How to get Map<String,String> from Future<Map<String,String>>
+        },
+      ),
+    );
   }
 }
