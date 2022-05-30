dbd36f1798a807a98c00b40a3bb93e327f2b57b3
malu1608 <mlflsalvador@gmail.com>
2022-05-30 14:24:49 +0100

    Feedback bar in progress

--- src/lib/cantina.dart
+++ src/lib/cantina.dart
@@ -1,8 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hello_world/main.dart';
-class MyApp extends StatelessWidget {
+
+void main() => runApp(const Cantina());
+
+class Cantina extends StatelessWidget {
   // This widget is the root
   // of your application.
+  const Cantina({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,86 +16,127 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: MyStatefulWidget(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
- 
-class Cantina extends StatelessWidget {
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final MediaQueryData queryData = MediaQuery.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        toolbarHeight: 150,
         actions: <Widget>[
-          Center(
-            child: MaterialButton(
-                height: 25,
-                onPressed: () {},
-                color: const Color.fromARGB(255, 210, 187, 128),
-                textColor: Colors.white,
-                child: const Icon(
-                  Icons.account_circle,
-                  size: 65,
-                ),
-                padding: const EdgeInsets.all(9),
-                shape: const CircleBorder()),
+          IconButton(
+            alignment: Alignment.topLeft,
+            color: Colors.grey,
+            icon: const Icon(Icons.account_circle, size: 40),
+            onPressed: () {},
           ),
         ],
       ),
-  body: Column(
-        children:<Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Current Occupancy",textScaleFactor: 2,style: TextStyle(fontWeight:FontWeight.bold),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: queryData.size.height * 0.1,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Table(
-               
-            textDirection: TextDirection.rtl,
-            defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
-            border:TableBorder.all(width: 2.0,color: Colors.red),
-            children: [
-              TableRow(
-                children: [
-                  Text("",textScaleFactor: 1.5,textAlign: TextAlign.center,),
-                  Text("Sopa",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                ]
+          const Text(
+            'Current Occupancy',
+            style: TextStyle(fontSize: 20),
+          ),
+          LinearProgressIndicator(
+            backgroundColor: Colors.grey,
+            value: controller.value,
+            semanticsLabel: 'Linear progress indicator',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MaterialButton(
+                onPressed: () {},
+                textColor: const Color.fromARGB(255, 139, 186, 118),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: <Widget>[
+                    Icon(Icons.sentiment_satisfied_alt_outlined, size: 50),
+                  ],
+                ),
               ),
-               TableRow(
-                children: [
-                  Text("",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                  Text("Carne",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                ]
-              ),
-              TableRow(
-                children: [
-                  Text("",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                  Text("Peixe",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                ]
-              ),
-              TableRow(
-                children: [
-                  Text("",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                  Text("Vegetariano",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                ]
-              ),
-              TableRow(
-                  children: [
-                    Text("",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                    Text("Dieta",textScaleFactor: 1.5,textAlign: TextAlign.center),
-                  ]
+              MaterialButton(
+                onPressed: () {},
+                textColor: const Color.fromARGB(255, 139, 186, 118),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: <Widget>[
+                    Icon(Icons.sentiment_dissatisfied_outlined, size: 50),
+                  ],
+                ),
               ),
             ],
-        ),
           ),
-        ]
+          Table(
+            textDirection: TextDirection.rtl,
+            defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+            border: TableBorder.all(width: 2.0, color: Colors.red),
+            children: const [
+              TableRow(children: [
+                Text(
+                  "",
+                  textScaleFactor: 1.5,
+                  textAlign: TextAlign.center,
+                ),
+                Text("Sopa", textScaleFactor: 1.5, textAlign: TextAlign.center),
+              ]),
+              TableRow(children: [
+                Text("", textScaleFactor: 1.5, textAlign: TextAlign.center),
+                Text("Carne",
+                    textScaleFactor: 1.5, textAlign: TextAlign.center),
+              ]),
+              TableRow(children: [
+                Text("", textScaleFactor: 1.5, textAlign: TextAlign.center),
+                Text("Peixe",
+                    textScaleFactor: 1.5, textAlign: TextAlign.center),
+              ]),
+              TableRow(children: [
+                Text("", textScaleFactor: 1.5, textAlign: TextAlign.center),
+                Text("Vegetariano",
+                    textScaleFactor: 1.5, textAlign: TextAlign.center),
+              ]),
+              TableRow(children: [
+                Text("", textScaleFactor: 1.5, textAlign: TextAlign.center),
+                Text("Dieta",
+                    textScaleFactor: 1.5, textAlign: TextAlign.center),
+              ]),
+            ],
+          ),
+        ],
       ),
     );
-  
   }
-
-  
 }
--- src/lib/food.dart
+++ src/lib/food.dart
@@ -12,101 +12,101 @@ class Food extends StatelessWidget {
           IconButton(
             alignment: Alignment.topLeft,
             color: Colors.grey,
-            icon: const Icon(Icons.account_circle, size:40),
+            icon: const Icon(Icons.account_circle, size: 40),
             onPressed: () {},
           ),
-          ],
+        ],
       ),
-      body:
-      GridView(
+      body: GridView(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 1,
           crossAxisSpacing: 7,
           mainAxisSpacing: 7,
           mainAxisExtent: 100,
-
         ),
         children: <Widget>[
           Container(
-            margin: EdgeInsets.fromLTRB(10,0,10,0),
-            child : SizedBox(
-            height: 60,
-            width: 250,
-            child:ElevatedButton(
-                child: const Text('AEFEUP', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 147, 77, 76)),
-                ),
-                onPressed: () {}
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SizedBox(
+              height: 60,
+              width: 250,
+              child: ElevatedButton(
+                  child: const Text('AEFEUP',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 147, 77, 76)),
+                  ),
+                  onPressed: () {}),
             ),
           ),
-    ),
-    Container(
-    margin: EdgeInsets.fromLTRB(10,0,10,0),
-    child : SizedBox(
-            child:ElevatedButton(
-                child: const Text('Bar da Biblioteca', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB( 255, 171, 105, 90)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SizedBox(
+              child: ElevatedButton(
+                  child: const Text('Bar da Biblioteca',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 171, 105, 90)),
+                  ),
+                  onPressed: () {}),
             ),
           ),
-    ),
-            Container(
-            margin: EdgeInsets.fromLTRB(10,0,10,0),
-            child : SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Cantina', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 176, 118, 105)),
-                ),
-                onPressed: () {Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => Cantina()),
-                );}
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child: ElevatedButton(
+                  child: const Text('Cantina',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 176, 118, 105)),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => Cantina()),
+                    );
+                  }),
             ),
           ),
-            ),
-            Container(
-            margin: EdgeInsets.fromLTRB(10,0,10,0),
-            child : SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Restaurante do INEGI', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 197, 162, 150)),
-                ),
-                onPressed: () {}
-            ),
-          ),
-            ),
-            Container(
-            margin: EdgeInsets.fromLTRB(10,0,10,0),
-            child : SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Bar de Minas', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 212, 186, 178)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child: ElevatedButton(
+                  child: const Text('Restaurante do INEGI',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 197, 162, 150)),
+                  ),
+                  onPressed: () {}),
             ),
           ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child: ElevatedButton(
+                  child: const Text('Bar de Minas',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 212, 186, 178)),
+                  ),
+                  onPressed: () {}),
             ),
+          ),
         ],
       ),
     );
