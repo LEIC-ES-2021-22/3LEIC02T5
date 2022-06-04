831d1feee07f9d900abe0f23799ade5ef485321f
splm02 <up202007675@edu.fc.up.pt>
2022-06-04 18:43:31 +0100

    working on library interface

--- src/lib/library.dart
+++ src/lib/library.dart
@@ -26,94 +26,100 @@ class Library extends StatelessWidget {
 
         ),
         children: <Widget>[
-          SizedBox(
-            height: 60,
-            width: 250,
-            child:ElevatedButton(
-                child: const Text('Piso 1', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255,107, 0, 0)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child : SizedBox(
+              height: 60,
+              width: 220,
+              child:ElevatedButton(
+                  child: const Text('6', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 190, 191, 191)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
-          SizedBox.fromSize(
-            size: Size(60,200),
-            child:ElevatedButton(
-                child: const Text('Piso 2', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB( 255, 141, 40,19)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child : SizedBox(
+              height: 60,
+              width: 250,
+              child:ElevatedButton(
+                  child: const Text('5', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB( 255, 158, 158, 159)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
-          SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Piso 3', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 139, 38, 16)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child : SizedBox(
+              height: 60,
+              width: 100,
+              child:ElevatedButton(
+                  child: const Text('4', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 140, 140, 141)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
-          SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Piso 4', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 191, 142, 127)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child:ElevatedButton(
+                  child: const Text('3', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 99, 99, 99)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
-          SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Piso 5', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 191, 142, 127)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child:ElevatedButton(
+                  child: const Text('2', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 50, 50, 50)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
-          SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Piso 6', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 191, 142, 127)),
-                ),
-                onPressed: () {}
-            ),
-          ),
-          SizedBox(
-            height: 60,
-            width: 100,
-            child:ElevatedButton(
-                child: const Text('Piso 7', style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromARGB(255, 191, 142, 127)),
-                ),
-                onPressed: () {}
+          Container(
+            margin: EdgeInsets.fromLTRB(10,20,300,0),
+            child: SizedBox(
+              height: 60,
+              width: 100,
+              child:ElevatedButton(
+                  child: const Text('1', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 2, 2, 2)),
+                  ),
+                  onPressed: () {}
+              ),
             ),
           ),
         ],
